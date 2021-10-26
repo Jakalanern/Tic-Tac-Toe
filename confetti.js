8781ad45abd388d020b5c76a4af8b93dcf980ce0
Jack Stevens <77250250+Jakalanern@users.noreply.github.com>
2021-10-26 13:44:19 -0700

    Add files via upload

--- confetti.js
+++ confetti.js
@@ -1,6 +1,6 @@
 var confetti = {
-  maxCount: 150, //set max confetti count
-  speed: 6, //set the particle animation speed
+  maxCount: 75, //set max confetti count
+  speed: 8, //set the particle animation speed
   frameInterval: 15, //the confetti animation frame interval in milliseconds
   alpha: 1.0, //the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
   gradient: false, //whether to use gradients for the confetti particles
@@ -16,6 +16,7 @@ var confetti = {
 };
 
 (function () {
+  console.log("CONFETTI");
   confetti.start = startConfetti;
   confetti.stop = stopConfetti;
   confetti.toggle = toggleConfetti;
